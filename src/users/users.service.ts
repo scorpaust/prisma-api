3627082500b1d1d315b75f527123f3ff5f7bdaf1
Dinis Costa <scorpaust@hotmail.com>
2022-11-06 23:33:11 +0000

    Prisma Api - Users Module

---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersRepository } from './repositories/users.repository';
+
+@Injectable()
+export class UsersService {
+
+  constructor(
+    private readonly usersRepository: UsersRepository
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.usersRepository.create(createUserDto);
+  }
+
+  findAll() {
+    return this.usersRepository.findAll();
+  }
+
+  findOne(id: number) {
+    return this.usersRepository.findOne(id);
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.usersRepository.update(id, updateUserDto);
+  }
+
+  remove(id: number) {
+    return this.usersRepository.remove(id);
+  }
+}
